c13b2a2a7ad74c4cb7ac04b9c12d105c5b19b6a4
Jonathan Bridge <jvbridge@gmail.com>
2022-05-27 14:14:39 -0700

    changes so far

--- src/components/Bucket.js
+++ src/components/Bucket.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
-import BucketForm from './BucketForm';
+import React, { useState } from "react";
+import BucketForm from "./BucketForm";
 
 function Bucket(props) {
+  const { editBucketItem, removeBucketItem, completeBucketItem } = props;
+
   const [edit, setEdit] = useState({
     id: null,
-    value: '',
-    eagerness: '',
+    value: "",
+    eagerness: "",
   });
 
   console.log(props.bucket);
 
   const submitUpdate = (value) => {
-
     // TODO: Write logic to update the `edit` value in state after a user updates an entry in the list
-
     // TODO: Set the key:value pairs in the `edit` object back to empty strings
-
   };
 
   // If the user is attempting to edit an item, render the bucket form with the edit variable passed as a prop
@@ -27,17 +26,25 @@ function Bucket(props) {
     // TODO: Add a className of `bucket row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
     // TODO: Add a key attribute set to the value of the index position
     // Hint: use a ternary operator
-    <div className={ } key={}>
-
-      // TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument
-      <div key={} onClick={}>
-          {/* TODO: Add the item text here */}
+    <div
+      className={
+        item.value
+          ? `bucket row complete ${item.eagerness}`
+          : `bucket-row ${item.eagerness}`
+      }
+      key={index}
+    >
+      // TODO: Add an onClick event that invokes the `completeBucketItem` method
+      passing the item id as a argument
+      <div key={index} onClick={() => completeBucketItem()}>
+        {item.value}
       </div>
       <div className="icons">
-        // TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties
-        <p onClick={}> ✏️</p>
+        // TODO: Add an onClick event update the `edit` object with the `id`,
+        `value`, and `eagerness` properties
+        <p onClick={() => editBucketItem(index)}> ✏️</p>
         {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
-        <p onClick={}> 🗑️</p>
+        <p onClick={() => removeBucketItem()}> 🗑️</p>
       </div>
     </div>
   ));
